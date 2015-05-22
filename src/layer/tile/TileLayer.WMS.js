ae63bc9430bc1b3bf364b2d87219e289f2d12e70
TolonUK <alex@tolon.co.uk>
2015-05-22 11:49:23 +0100

    Fix to refreshtime

--- src/layer/tile/TileLayer.WMS.js
+++ src/layer/tile/TileLayer.WMS.js
@@ -66,7 +66,7 @@ L.TileLayer.WMS = L.TileLayer.extend({
 
 		if (!(this.options.maxTileAgeMsBeforeRefresh < 0)) {
 			var epoch = new Date().getTime();
-			if (this.options.maxTileAgeMsBeforeRefresh > 0) { epoch = Math.floor(epoch / this.options.maxTileAgeMsBeforeRefresh) * this.maxTileAgeMsBeforeRefresh; }
+			if (this.options.maxTileAgeMsBeforeRefresh > 0) { epoch = Math.floor(epoch / this.options.maxTileAgeMsBeforeRefresh) * this.options.maxTileAgeMsBeforeRefresh; }
 			url += '&refreshtime=' + epoch;
 		}
 
